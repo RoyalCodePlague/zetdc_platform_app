24a38cad45b034d0d05f0d98c7c675d1159c7388
Royal_Code_Plague <55633556+RoyalCodePlague@users.noreply.github.com>
2025-11-09 18:28:34 +0200

    Update settings.py
    Added 
    TEMPLATES = [
        {
            ...
        },
    ]

--- backend/settings.py
+++ backend/settings.py
@@ -147,6 +147,24 @@ else:
         }
     }
 
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # you can change this path if needed
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 # ==============================
 # Miscellaneous
 # ==============================
